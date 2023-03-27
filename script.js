760a8e2b68e2bf3a2676055013829329208a4c07
Jones Souza <108988209+jonessouzasilva@users.noreply.github.com>
2023-03-27 20:07:20 -0300

    commit

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const sr = ScrollReveal ({
     reset: true
 });
 
-sr.reveal('home-text', {delay:200, origin:'left'});
-sr.reveal('home-img', {delay:200, origin:'right'});
+sr.reveal('.home-text', {delay:200, origin:'left'});
+sr.reveal('.home-img', {delay:200, origin:'right'});
 
 sr.reveal('.container, .about, .menu,  .contact' ,{delay:200, origin:'bottom'});
