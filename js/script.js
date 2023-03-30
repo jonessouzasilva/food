e44ebf2ccb00ea7b4201ffca1766b065598a2994
Jones Souza <108988209+jonessouzasilva@users.noreply.github.com>
2023-03-30 16:03:57 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const header = document.querySelector("header");
 
-window.addEventListener("scroll", fuction() {
-    header.classList.toggle("sticky", window.scrolly > 0);
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
